80fc5a0029126ba565b6ce44f4cc110fc0f91801
OnyxAzryn <brycekabat@onyxazryn.com>
2025-06-14 11:45:28 -0500

    Debug /usr/etc issue

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,9 +16,9 @@ dnf install -y kernel-cachyos-server
 dnf -y copr disable bieszczaders/kernel-cachyos
 
 # Install CachyOS Kernel Addons
-#dnf -y copr enable bieszczaders/kernel-cachyos-addons
-#dnf install -y cachyos-ksm-settings scx-scheds
-#dnf -y copr disable bieszczaders/kernel-cachyos-addons
+dnf -y copr enable bieszczaders/kernel-cachyos-addons
+dnf install -y cachyos-ksm-settings scx-scheds
+dnf -y copr disable bieszczaders/kernel-cachyos-addons
 
 # Disable ZFS
 echo "omit_dracutmodules+=\" zfs \"" | tee -a /etc/dracut.conf.d/omit-zfs.conf
@@ -27,5 +27,7 @@ echo "omit_dracutmodules+=\" zfs \"" | tee -a /etc/dracut.conf.d/omit-zfs.conf
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-cachyos-server-(\d+)' | sed -E 's/kernel-cachyos-server-//')"
 dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 /lib/modules/$QUALIFIED_KERNEL/initramfs.img
+
+ls -la /usr/etc
 
 #systemctl enable podman.socket
